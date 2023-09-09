ba61e033afa613e9f2d4e7c1a88142185ea0f8c0
Wheeze_NL <github@uberia.nl>
2023-09-09 15:35:10 +0200

    Complete rewrite

--- generate_receipt_from_arg.py
+++ generate_receipt_from_arg.py
@@ -2,8 +2,15 @@ import sys, os
 import json
 from escpos.printer import Serial
 from escpos.printer import Network
-from PIL import Image, ImageOps, ImageDraw
+from PIL import Image, ImageOps, ImageDraw, ImageFont, ImageFilter
 from datetime import datetime
+
+# Variables
+filename = "tmpbadge.png"
+headerheight = 180
+finalRun = False
+totalHeight = 8000
+maxWidth = 512
 
 # Disable Print
 def blockPrint():
@@ -13,7 +20,64 @@ def blockPrint():
 def enablePrint():
     sys.stdout = sys.__stdout__
 
-def print_frituur_receipt(message):
+def prepare_receipt(message):
+    runs = 0
+    totalHeight = 8000
+    data = json.loads(message)
+    account = data["user"]
+    items = data["items"]
+    
+    while runs<2:
+        now = datetime.now()
+        current_date = now.strftime("%Y-%m-%d")
+        current_time = now.strftime("%H:%M:%S")
+        if(runs):
+            img = Image.new('RGB', size=(512, totalHeight),color='white')
+        else:
+            img = Image.new('RGB', size=(512, totalHeight))
+        draw = ImageDraw.Draw(img)
+
+        orderfromfont = ImageFont.truetype("./tff/Arialn.ttf", 30)
+        nickfont = ImageFont.truetype("./tff//ArialNarrowBold.ttf", 50)
+        timefont = ImageFont.truetype("./tff//Arialn.ttf", 30)
+        headerfont = ImageFont.truetype("./tff//ArialNarrowBold.ttf", 30)
+        font = ImageFont.truetype("./tff//Arialn.ttf", 40)
+
+        #if(len(account)>12):  account=account[0:12]
+
+        # Header
+        draw.text([0,0],'Bestelling voor:',fill= 'black',font=orderfromfont)
+        draw.text([0,40],account,fill= 'black',font=nickfont)
+        draw.text([0,headerheight-80],current_date + " "+ current_time,fill= 'black',font=timefont)
+        draw.text([0,headerheight-40],'Snack',fill= 'black',font=headerfont)
+        draw.text([280,headerheight-40],'In Pan  Uitgeleverd',fill= 'black',font=headerfont)
+        draw.line((0, headerheight, maxWidth, headerheight),fill='black',width=3)
+
+        pi4dec = Image.open('img_pi4dec.png')
+        img.paste(pi4dec,[maxWidth - 140,20])
+
+        # Items
+        regels = 0
+        for item in items:
+            quantity = str(item["quantity"])
+            name = item["description"].split("(Frituur) ",1)[1]
+            if (quantity > '1'):
+                completeItem = quantity + "x " + name
+            else:
+                completeItem = name
+            if(len(completeItem)>16):
+                completeItem = completeItem + "\n\t"
+            draw.text([0,headerheight+(regels*45)],completeItem,fill= 'black',font=font)
+            draw.rounded_rectangle([280,headerheight+(regels*45)+10,310,headerheight+(regels*45)+40],5,'white','black',3)
+            draw.rounded_rectangle([370,headerheight+(regels*45)+10,400,headerheight+(regels*45)+40],5,'white','black',3)
+            regels = regels+1
+        draw = ImageDraw.Draw(img)
+        test1,test2,test3,totalHeight = img.getbbox()
+        totalHeight = totalHeight + 10
+        runs = runs + 1
+    img.save(filename, "PNG")
+
+def print_frituur_receipt():
     blockPrint()
     """ 9600 Baud, 8N1, Flow Control Enabled """
     '''p = Serial(devfile='/dev/ttyUSB0',
@@ -24,41 +88,16 @@ def print_frituur_receipt(message):
         timeout=1.00,
         dsrdtr=True
         )'''
-    p = Network("192.168.192.168")
+    p = Network("192.168.192.168",profile='TM-T88V')
     enablePrint()
-    data = json.loads(message)
-    account = data["user"]
-    items = data["items"]
-
-    now = datetime.now()
-
-    p.set(align='left', font='a', bold=True, underline=0, width=1, height=1, density=9, invert=False, smooth=False, flip=False, double_width=False, double_height=True, custom_size=False)
-    p.text("Bestelling voor: ")
-    if len(account)>12 : p.text("\n")
-    p.set(align='left', font='a', bold=True, underline=0, width=1, height=1, density=9, invert=False, smooth=False, flip=False, double_width=True, double_height=True, custom_size=False)
-    p.text(account + "\n")
-
-    p.set(align='left', font='a', bold=False, underline=0, width=1, height=1, density=9, invert=False, smooth=False, flip=False, double_width=False, double_height=False, custom_size=False)
-    current_time = now.strftime("%H:%M:%S")
-    p.text("Besteld om: "+ current_time +"\n\n")
-
-    p.set(align='left', font='a', bold=True, underline=1, width=1, height=1, density=9, invert=False, smooth=False, flip=False, double_width=False, double_height=True, custom_size=False)
-    p.text("Snack\t\tIn Pan\tUitgeleverd\n")
-
-    p.set(align='left', font='a', bold=False, underline=0, width=1, height=2, density=9, invert=False, smooth=False, flip=False, double_width=False, double_height=True, custom_size=False)
-    for item in items:
-        quantity = str(item["quantity"])
-        name = item["description"].split("(Frituur) ",1)[1]
-        completeItem = quantity + "x: " + name
-        if(len(completeItem)>16):
-            completeItem = completeItem + "\n\t"
-        p.text(completeItem + "\t[  ]\t[  ]\n")
+    p.image("tmpbadge.png")
     p.cut()
 
 #    print("Receipt sent to printer")
 
 if len(sys.argv)>1: #print accountname and firstname
     print("\nIk stuur de frituurbestelling naar de printer!\n")
-    print_frituur_receipt(sys.argv[1])
+    prepare_receipt(sys.argv[1])
+    print_frituur_receipt()
 else: #give a nice prompt
     print("So, I was hoping for some JSON, but got none")
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/lennart/git/generate_receipt/
+cd /home/lennart/git/Frituur_Receipt_Generator/
 python3 generate_receipt_from_arg.py "$@"
